08106d73812c5ddbc51af156f69a91fd6abd5d2f
6513177-Ananda <ananda.kon@student.mahidol.edu>
2024-06-17 08:57:56 +0700

    Add ui into rps

--- Project/rock-paper-scissors/script.js
+++ Project/rock-paper-scissors/script.js
@@ -1,3 +1,11 @@
+// Todo List
+// When clicked, box will be green if win and red if lose
+// POP UP win or lose (Can be done by use dialog or just use alert)
+
+const btn = document.querySelectorAll("#btn");
+const playerScoreText = document.querySelector("#playerScore");
+const computerScoreText = document.querySelector("#computerScore");
+
 let map = new Map([
     ["scissor", 2],
     ["paper", 1],
@@ -20,20 +28,21 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let humanInput = prompt("Rock Paper Scissor!!!").toLowerCase();
     let humanMap = map.get(humanInput) ?? getHumanChoice();
     return humanMap;
 }
 
-function playRound() {
-    if (result > 2 || computerScore >= 5 || humanScore >= 5) {play = false; return;}
+function playRound(result, humanResult, botResult) {
+    // if (result > 2 || computerScore >= 5 || humanScore >= 5) {play = false; return;}
     if (result === 1 || result === -2) {
         console.log(`Human Won: ${reverseMap.get(humanResult)} x ${reverseMap.get(botResult)}`);
         humanScore++;
+        playerScoreText.textContent = humanScore;
     } else if (result == 0){
         console.log(`Human Draw: ${reverseMap.get(humanResult)} x ${reverseMap.get(botResult)}`)
     } else {
         computerScore++;
+        computerScoreText.textContent = computerScore;
         console.log(`Human Lost: ${reverseMap.get(humanResult)} x ${reverseMap.get(botResult)}`)
     }
 }
@@ -44,12 +53,21 @@ function showScore() {
     console.log(finalLog);
 }
 
-while (play) {
-    var botResult = getComputerChoice();
-    var humanResult = getHumanChoice();
-    var result = humanResult - botResult;
-    playRound(result);
-}
+// while (play) {
+//     var botResult = getComputerChoice();
+//     var humanResult = getHumanChoice();
+//     var result = humanResult - botResult;
+//     playRound(result);
+// }
+
+btn.forEach(button => {
+    button.addEventListener("click", (e) => {
+        let playerInput = map.get(e.target.innerText.toLowerCase());
+        let botInput = getComputerChoice();
+        let result = playerInput - botInput
+        playRound(result, playerInput, botInput);
+    });
+});
 
 showScore();
 
