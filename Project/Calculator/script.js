3cd7de7b348be4fed37ca0face1ecac12b7e4474
6513177-Ananda <ananda.kon@student.mahidol.edu>
2024-06-24 18:58:35 +0700

    Finished with poor design

--- Project/Calculator/script.js
+++ Project/Calculator/script.js
@@ -1,13 +1,22 @@
 const computeFrame = document.querySelector("#compute-frame");
-let boxFrame, box, currentType;
-let boxArray;
+const resultCurrent = document.querySelector("#result-text-current");
+const resultPrev = document.querySelector("#result-text-prev");
+let boxFrame, box, boxArray; 
+let currentType, currentOperation = '=';
+let value, x, y;
+let recursive = false;
+const typeArrayFunction = ['<', '/', '*', '-', '+', '%', 'c', 'ce', '='];
+const typeArrayFunctionStay = ['<', 'c', 'ce', '='];
+const typeArrayFunctionClear = ['c', 'ce'];
 const typeArray = [
     'c', 'ce', '%', '<', 
     '7', '8', '9', '/', 
     '4', '5', '6', '*', 
     '1', '2', '3', '-',
-    '+-', '0', '.', '+'
+    '.', '0', '=', '+'
 ];
+
+
 
 const createButton = ()=> {
     for (let i = 0; i < 5; i++){
@@ -26,18 +35,118 @@ const createButton = ()=> {
     boxArray = document.querySelectorAll("button");
 }
 
-const calculate = ()=> {
-
+const getMouseInput = ()=> {
+    boxArray.forEach(element => {
+        element.addEventListener("click", (e)=> {
+            updateScreen(e);
+        })
+    });
 }
 
-const plus = ()=> {
+const updateScreen = (e)=> {
+    if (typeArrayFunction.includes(e.target.textContent)){
+        if (typeArrayFunctionClear.includes(e.target.textContent)){
+            calculate(e.target.textContent);
+        }
+        else {
+            if (resultPrev.textContent === "" && !typeArrayFunctionStay.includes(e.target.textContent)) {
+                PrevResultNull();
+                currentOperation = e.target.textContent;
+            }
+            else{
+                console.log(currentOperation);
+                calculate(currentOperation);
+                currentOperation = e.target.textContent;
+            }
+        }
+    }
+    else {
+        draw(e.target)
+    }
+}    
 
+const draw = (textNode) => {
+    resultCurrent.textContent += textNode.textContent;
 }
 
-const minus = ()=> {
-
+const PrevResultNull = ()=> {
+    if (resultPrev.textContent === ""){
+        resultPrev.textContent = resultCurrent.textContent;
+        resultCurrent.textContent = "";
+        return;
+    }
 }
 
-createButton()
+const calculate = (type)=> {
 
+    x = Number(resultPrev.textContent);
+    y = Number(resultCurrent.textContent);
+
+    switch (type){
+        case '+':
+            value = plus(x, y);
+            break;
+        case '-':
+            value = minus(x, y);
+            break;
+        case '*':
+            value = multiply(x, y);
+            break;
+        case '/':
+            value = divide(x, y);
+            break;
+        case '%':
+            value = mod(x, y);
+            break;
+        case 'c':
+            resultCurrent.textContent = "";
+            resultPrev.textContent = "";
+            return;
+        case 'ce':
+            resultCurrent.textContent = "";
+            return;
+        case '=':
+            if (!recursive) {
+                recursive = true;
+                calculate(type);
+            }
+            break;
+        case '<':
+            popString(resultCurrent.textContent);
+            return;
+    }
+    resultPrev.textContent = value;
+    resultCurrent.textContent = "";
+}
+
+const popString = (e) => {
+    let textString = e.toString();
+    textString = textString.substring(0, textString.length - 1);
+    console.log(textString);
+    resultCurrent.textContent = textString;
+}
+
+const plus = (x, y)=> {
+    return x + y;
+}
+
+const minus = (x, y)=> {
+    return x - y;
+}
+
+const multiply = (x, y) => {
+    return x * y;
+}
+
+const divide = (x, y) => {
+    return x/y;
+}
+
+const mod = (x, y) => {
+    return x % y;
+}
+
+
+createButton();
+getMouseInput();
 
